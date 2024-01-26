30be6701016ed1aaedcd0aa4107e4caa19b16f5b
Honeybadger <rajimoshood200482@gmail.com>
2024-01-26 16:55:51 +0100

    created search field

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,17 @@
-import { StyleSheet, View, Text, SafeAreaView, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Alert,
+  Pressable,
+  Image,
+  TextInput,
+} from "react-native";
 import React, { useEffect, useState } from "react";
+import { Feather } from "@expo/vector-icons";
 import * as Location from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const [displayCurrentAddress, setdisplayCurrentAddress] = useState(
@@ -70,7 +81,41 @@ const HomeScreen = () => {
   };
   return (
     <SafeAreaView>
-      <Text>let's go</Text>
+      {/* Location and Profile */}
+      <View>
+        <MaterialIcons name="location-on" size={30} color="#fd5c63" />
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: "600" }}>Home</Text>
+          <Text>{displayCurrentAddress}</Text>
+        </View>
+        <Pressable
+          onPress={() => navigation.navigate("Profile")}
+          style={{ marginLeft: "auto", marginRight: 7 }}
+        >
+          <Image
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+            source={{
+              uri: "https://lh3.googleusercontent.com/ogw/AAEL6sh_yqHq38z35QMy5Fnb8ZIxicdxCIVM9PeBD2j-=s64-c-mo",
+            }}
+          />
+        </Pressable>
+      </View>
+      {/* Search Bar */}
+      <View
+        style={{
+          padding: 10,
+          margin: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderWidth: 0.8,
+          borderColor: "#C0C0C0",
+          borderRadius: 7,
+        }}
+      >
+        <TextInput placeholder="Search for items or More" />
+        <Feather name="search" size={24} color="#fd5c63" />
+      </View>
     </SafeAreaView>
   );
 };
