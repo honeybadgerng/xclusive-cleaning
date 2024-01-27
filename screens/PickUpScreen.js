c928b4762ecfb00ebebd24ba89d9a597f7c64aef
Honeybadger <rajimoshood200482@gmail.com>
2024-01-28 00:34:21 +0100

    created input field for address

--- screens/PickUpScreen.js
+++ screens/PickUpScreen.js
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
 import React from "react";
 
 const PickUpScreen = () => {
   return (
-    <View>
-      <Text>PickUpScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+        enter Address
+      </Text>
+      <TextInput
+        style={{
+          padding: 40,
+          borderColor: "gray",
+          borderWidth: 0.7,
+          paddingVertical: 80,
+          borderRadius: 9,
+          margin: 10,
+        }}
+      />
+      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+        Pick Up Date
+      </Text>
+    </SafeAreaView>
   );
 };
 
