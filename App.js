a146ca741deb37c7b7ef01a13bc0ded6c19ea433
Honeybadger <rajimoshood200482@gmail.com>
2024-01-27 20:15:05 +0100

    initialized react redux and react redux toolkit

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
+import { Provider } from "react-redux";
+import store from "./store";
 // import Carousel from "./components/Carousel";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Provider store={store}>
       <HomeScreen />
-      {/* <Carousel /> */}
       <StatusBar style="auto" />
-    </View>
+    </Provider>
   );
 }
 
