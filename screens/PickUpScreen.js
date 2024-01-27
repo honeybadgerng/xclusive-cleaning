8cf00b8ef71d878d0ec9fe2cae34102314a063e6
Honeybadger <rajimoshood200482@gmail.com>
2024-01-28 00:57:05 +0100

    added delivery time and time array

--- screens/PickUpScreen.js
+++ screens/PickUpScreen.js
@@ -4,6 +4,55 @@ import HorizontalDatepicker from "@awrminkhodaei/react-native-horizontal-datepic
 
 const PickUpScreen = () => {
   const [selectedDate, setSelectedDate] = useState("");
+  const deliveryTime = [
+    {
+      id: "0",
+      name: "2-3 Days",
+    },
+    {
+      id: "1",
+      name: "3-4 Days",
+    },
+    {
+      id: "2",
+      name: "4-5 Days",
+    },
+    {
+      id: "3",
+      name: "5-6 Days",
+    },
+    {
+      id: "4",
+      name: "Tommorrow",
+    },
+  ];
+
+  const times = [
+    {
+      id: "0",
+      time: "11:00 PM",
+    },
+    {
+      id: "1",
+      time: "12:00 PM",
+    },
+    {
+      id: "2",
+      time: "1:00 PM",
+    },
+    {
+      id: "2",
+      time: "2:00 PM",
+    },
+    {
+      id: "4",
+      time: "3:00 PM",
+    },
+    {
+      id: "5",
+      time: "4:00 PM",
+    },
+  ];
   return (
     <SafeAreaView>
       <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
@@ -38,6 +87,9 @@ const PickUpScreen = () => {
         unselectedItemBackgroundColor="#ececec"
         flatListContainerStyle={styles.flatListContainerStyle}
       />
+      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+        Select time
+      </Text>
     </SafeAreaView>
   );
 };
