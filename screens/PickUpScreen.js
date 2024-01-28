632573416794185c0fefa4908bd725190b8ff2f2
Honeybadger <rajimoshood200482@gmail.com>
2024-01-28 02:24:43 +0100

    added styling to fit to screen

--- screens/PickUpScreen.js
+++ screens/PickUpScreen.js
@@ -97,7 +97,7 @@ const PickUpScreen = () => {
   };
   return (
     <>
-      <SafeAreaView>
+      <SafeAreaView style={{ marginTop: 50 }}>
         <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
           enter Address
         </Text>
