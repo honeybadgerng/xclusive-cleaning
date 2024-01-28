eb26979269a5e6975ca1ac8e7e8832c7c70fa5b1
Honeybadger <rajimoshood200482@gmail.com>
2024-01-28 02:33:18 +0100

    Created condition to render on Cart screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,11 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CartScreen = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  const total = cart
+    .map((item) => item.quantity * item.price)
+    .reduce((curr, prev) => curr + prev, 0);
   return (
-    <View>
+    <ScrollView style={{ marginTop: 50 }}>
+      {total === 0 ? (
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ marginTop: 40 }}>Your cart is empty</Text>
+        </View>
+      ) : (
+        <></>
+      )}
+
       <Text>CartScreen</Text>
-    </View>
+    </ScrollView>
   );
 };
 
